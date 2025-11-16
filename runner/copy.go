54ad56f30336dfa8258270f058824f34fa9d05fc
Léo Riera <leo.riera1@gmail.com>
2025-11-16 14:22:32 +0100

    copy files and directories recursively

--- runner/copy.go
+++ runner/copy.go
@@ -2,8 +2,11 @@ package runner
 
 import (
 	"fmt"
+	"io"
+	"io/fs"
 	"os"
 	"path"
+	"path/filepath"
 
 	"github.com/Dowdow/gosible/utils"
 	tea "github.com/charmbracelet/bubbletea"
@@ -22,26 +25,59 @@ func (a *CopyArgs) Validate() bool {
 }
 
 func (a *CopyArgs) Run(session *ssh.Session, ch chan tea.Msg) error {
-	content, err := os.ReadFile(a.Src)
-	if err != nil {
-		return fmt.Errorf("[copy] %v\n", err)
-	}
-
 	stdin, err := session.StdinPipe()
 	if err != nil {
 		return fmt.Errorf("[copy] %v\n", err)
 	}
 	defer stdin.Close()
 
-	cmd := fmt.Sprintf("scp -t %s", path.Dir(a.Dest))
+	cmd := fmt.Sprintf("scp -tr %s", path.Dir(a.Dest))
 	if err := session.Start(cmd); err != nil {
 		return fmt.Errorf("[copy] %v\n", err)
 	}
 
-	fmt.Fprintf(stdin, "C0644 %d %s\n", len(content), path.Base(a.Dest))
-	stdin.Write(content)
-	fmt.Fprint(stdin, "\x00")
+	err = filepath.WalkDir(a.Src, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		rel, err := filepath.Rel(filepath.Dir(a.Src), path)
+		if err != nil {
+			return err
+		}
+
+		info, err := d.Info()
+		if err != nil {
+			return err
+		}
+
+		// Directory
+		if d.IsDir() {
+			if rel != "." {
+				fmt.Fprintf(stdin, "D%04o 0 %s\n", info.Mode().Perm(), filepath.Base(path))
+			}
+			return nil
+		}
+
+		// File
+		size := info.Size()
+		fmt.Fprintf(stdin, "C%04o %d %s\n", info.Mode().Perm(), size, filepath.Base(path))
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		io.Copy(stdin, f)
+		f.Close()
+		fmt.Fprint(stdin, "\x00")
+
+		return nil
+	})
+	fmt.Fprint(stdin, "E\n")
 	stdin.Close()
+
+	if err != nil {
+		return fmt.Errorf("[copy] %v\n", err)
+	}
 
 	if err := session.Wait(); err != nil {
 		return fmt.Errorf("[copy] %v\n", err)
